38fe2ce63dd03cb5874580c2d20e4c6f75afa4de
jerry nabango <jerrynabango9@gmail.com>
2024-02-06 12:33:40 +0300

    basic flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -6,7 +6,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route("/")
 def basic():
     """HTML rendering"""
     return render_template("0-index.html")
