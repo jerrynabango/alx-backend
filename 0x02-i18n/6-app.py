2cdb2e0d95652d924a59ff4ca18a64de95c9d5ae
jerry nabango <jerrynabango9@gmail.com>
2024-02-06 17:43:26 +0300

    user locale

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -1,31 +1,20 @@
 #!/usr/bin/env python3
-"""
-A Basic flask application
-"""
-from typing import (
-    Dict, Union
-)
+"""Use user locale"""
 
-from flask import Flask
-from flask import g, request
-from flask import render_template
+from flask import Flask, render_template, request, g
 from flask_babel import Babel
+from typing import Dict, Union
 
 
 class Config(object):
-    """
-    Application configuration class
-    """
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    """Configuration"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-# Instantiate the application object
 app = Flask(__name__)
 app.config.from_object(Config)
-
-# Wrap the application with Babel
 babel = Babel(app)
 
 
@@ -38,25 +27,17 @@ users = {
 
 
 def get_user(id) -> Union[Dict[str, Union[str, None]], None]:
-    """
-    Validate user login details
-    Args:
-        id (str): user id
-    Returns:
-        (Dict): user dictionary if id is valid else None
-    """
+    """User"""
     return users.get(int(id), {})
 
 
 @babel.localeselector
 def get_locale() -> str:
-    """
-    Gets locale from request object
-    """
+    """Request"""
     options = [
-        request.args.get('locale', '').strip(),
-        g.user.get('locale', None) if g.user else None,
-        request.accept_languages.best_match(app.config['LANGUAGES']),
+        request.args.get("locale", '').strip(),
+        g.user.get("locale", None) if g.user else None,
+        request.accept_languages.best_match(app.config["LANGUAGES"]),
         Config.BABEL_DEFAULT_LOCALE
     ]
     for locale in options:
@@ -66,19 +47,15 @@ def get_locale() -> str:
 
 @app.before_request
 def before_request() -> None:
-    """
-    Adds valid user to the global session object `g`
-    """
-    setattr(g, 'user', get_user(request.args.get('login_as', 0)))
+    """Request"""
+    setattr(g, "user", get_user(request.args.get("login_as", 0)))
 
 
-@app.route('/', strict_slashes=False)
-def index() -> str:
-    """
-    Renders a basic html template
-    """
-    return render_template('6-index.html')
+@app.route("/")
+def basic():
+    """HTML rendering"""
+    return render_template("5-index.html")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
