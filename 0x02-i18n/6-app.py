2ec82c1fd3be08b0c6a0f7a9bbaf7a8c1d4e70bf
jerry nabango <jerrynabango9@gmail.com>
2024-02-06 17:27:15 +0300

    user locale

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 """Use user locale"""
 
-from flask import Flask, render_template, request, g
+from typing import (
+    Dict, Union
+)
+
+from flask import Flask
+from flask import g, request
+from flask import render_template
 from flask_babel import Babel
-from typing import Dict, Union
 
 
 class Config(object):
